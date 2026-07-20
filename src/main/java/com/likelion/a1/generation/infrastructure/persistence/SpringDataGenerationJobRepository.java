e516f5334ccb7794ae6610518c50fe312384b34c
tory <chkoreakr@google.com>
2026-07-20 14:04:58 +0900

    chore : 계측 데이터

--- src/main/java/com/likelion/a1/generation/infrastructure/persistence/SpringDataGenerationJobRepository.java
+++ src/main/java/com/likelion/a1/generation/infrastructure/persistence/SpringDataGenerationJobRepository.java
@@ -1,6 +1,9 @@
 package com.likelion.a1.generation.infrastructure.persistence;
 
 import com.likelion.a1.generation.domain.model.GenerationJob;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface SpringDataGenerationJobRepository extends JpaRepository<GenerationJob, Long> {}
+interface SpringDataGenerationJobRepository extends JpaRepository<GenerationJob, Long> {
+  List<GenerationJob> findByStatusIn(List<String> statuses);
+}
